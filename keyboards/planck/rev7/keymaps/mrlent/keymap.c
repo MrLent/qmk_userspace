9ffd4f3d2bc43e2280a09d5c4dd8aee5ccf3d54c
Mr_Lent <sebastien.pairault@gmail.com>
2025-03-09 17:40:34 +0100

    cleanup(planck): remove MIDI feature

--- keyboards/planck/rev7/keymaps/mrlent/keymap.c
+++ keyboards/planck/rev7/keymaps/mrlent/keymap.c
@@ -115,36 +115,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-/* clang-format off */
-float melody[8][2][2] = {
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-};
-/* clang-format on */
-
-#define JUST_MINOR_THIRD 1.2
-#define JUST_MAJOR_THIRD 1.25
-#define JUST_PERFECT_FOURTH 1.33333333
-#define JUST_TRITONE 1.42222222
-#define JUST_PERFECT_FIFTH 1.33333333
-
-#define ET12_MINOR_SECOND 1.059463
-#define ET12_MAJOR_SECOND 1.122462
-#define ET12_MINOR_THIRD 1.189207
-#define ET12_MAJOR_THIRD 1.259921
-#define ET12_PERFECT_FOURTH 1.33484
-#define ET12_TRITONE 1.414214
-#define ET12_PERFECT_FIFTH 1.498307
-
 deferred_token tokens[8];
-
-uint32_t reset_note(uint32_t trigger_time, void *note) {
-    *(float*)note = 440.0f;
-    return 0;
-}
