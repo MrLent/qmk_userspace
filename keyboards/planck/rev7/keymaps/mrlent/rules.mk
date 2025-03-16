fb906597abf073933243f6808bdf61619a6ae5a2
Mr_Lent <sebastien.pairault@gmail.com>
2025-03-16 14:40:08 +0100

    build(planck): add a base option set
    Add few options disabled and enabled with a clear separation between what
    was present by default and what is added.

--- keyboards/planck/rev7/keymaps/mrlent/rules.mk
+++ keyboards/planck/rev7/keymaps/mrlent/rules.mk
@@ -1,1 +1,14 @@
+# Default planck/rev7 option
 DEFERRED_EXEC_ENABLE = yes
+
+# Debug options
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = yes
+
+# Custom options
+AUDIO_ENABLE = yes
+ENCODER_ENABLE = no
+LTO_ENABLE = no
+MUSIC_ENABLE = no
+NKRO_ENABLE = no
+RGBLIGHT_ENABLE = yes
